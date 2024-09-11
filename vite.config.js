44d8c92324ac891f9900c33efb8578a1ff726930
T_c <miguelito.vargasf@gmail.com>
2024-09-11 11:57:13 -0500

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/AR-instrument',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
